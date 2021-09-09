c527c8179b926f1f61d66e844bb0dba9d8f77c71
Adriano Santos <adrian.sbueno@gmail.com>
2021-09-08 22:16:23 -0300

    Meu primeiro commit

---
+++ index.js
@@ -0,0 +1,31 @@
+let listaPecas = []
+let nomePeca = "Peça teste"
+let pesoPeca = 300
+let podeCadastrar = true
+
+if (pesoPeca < 100){
+    console.log("Não há possibilidade de cadastrar")
+    podeCadastrar = false
+}
+
+if (listaPecas.length > 10){
+    console.log("Limite de peças atingido")
+    podeCadastrar = false
+}
+
+if (nomePeca.length < 3){
+    console.log("Tamanho de letras inferior ao permitido")
+    podeCadastrar = false
+}
+
+if (podeCadastrar){
+    listaPecas.push(nomePeca)
+    console.log("Cadastramento efetuado com sucesso")
+}else{
+    console.log("Houve problemas para cadastrar a peça")
+}
+
+console.log("Listando as pecas cadastradas: ")
+for (let i=0; i<listaPecas.length; i++){
+    console.log(listaPecas[i])
+}
